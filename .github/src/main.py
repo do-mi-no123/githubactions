c0d8a9b17bcbd2a12367e07add08ce8ea76fb82c
do-mi-no123 <sakata.aoi.19971229@gmail.com>
2024-06-05 21:51:50 +0900

    Create main.py

---
+++ .github/src/main.py
@@ -0,0 +1,13 @@
+import sys
+
+def add(a: int,b: int) -> int:
+  return a + b
+
+def main():
+  if len(sys.argv) <= 2:
+    print("a + b is required")
+  _, a, b, *_ = sys.argv
+  print(add(*map(int,(a, b))))
+
+if __name__ == "__main__":
+  main()
